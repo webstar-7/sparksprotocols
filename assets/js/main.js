95804c90428383ba97c1894188b424f2abef0d99
webstar-7 <expitoderoy@gmail.com>
2021-09-09 02:44:14 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,7 +219,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "1";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
@@ -235,19 +235,19 @@ function showSlides(n) {
   
   
   
- var slideIndex = 1;
+ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "1";
+    slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 } 
   
   
